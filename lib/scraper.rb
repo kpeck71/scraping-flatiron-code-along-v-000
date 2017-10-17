26e838494226345d84f93ad336a58d0652b91f82
Kristen Peck <kpeck71@gmail.com>
2017-10-17 18:18:50 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,7 +16,7 @@ class Scraper
     end
 
     def get_page
-      Nokogiri::HTML(html)
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     end
 
     def get_courses
